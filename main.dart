06751117a05ed6aa7ac3172ed80ba70b218e1a6e
Karina Amador <akarina.at@gmail.com>
2020-10-07 23:46:31 -0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: HomePage(),
+)
+  );
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.red,
+        title: Text('Diz'),
+        actions: <Widget>[
+          new IconButton(icon: Icon(Icons.search, color: Colors.white,), onPressed:(){}),
+          new IconButton(icon: Icon(Icons.shopping_cart, color: Colors.white,), onPressed:(){})
+        ],
+      ),
+      drawer: new Drawer(
+        child: new ListView(
+          children: <Widget>[
+            //header
+            new UserAccountsDrawerHeader(
+                accountName: Text('Karina Amador'),
+                accountEmail: Text('akarina.at@gmail.com'),
+              currentAccountPicture: GestureDetector(
+                child: new CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  child: Icon(Icons.person, color: Colors.white,),
+                ),
+              ),
+              decoration: new BoxDecoration(
+                color: Colors.blueGrey,
+              ),
+            ),
+            //BODY
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Inicio'),
+                leading: Icon(Icons.home)
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                  title: Text('Mi cuenta'),
+                  leading: Icon(Icons.person)
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                  title: Text('Comprar por departamento'),
+                  leading: Icon(Icons.dashboard)
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                  title: Text('Promociones'),
+                  leading: Icon(Icons.star)
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
